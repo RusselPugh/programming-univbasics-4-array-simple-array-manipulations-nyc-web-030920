190396896903373a60c44d9cb768578716b61da7
Russel Pugh <russ_scooby@yahoo.com>
2020-02-28 06:44:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -12,7 +12,7 @@ end
 
 def using_pop(continents)
   continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-  deleted_string = continents.pop
+  continents.pop
 end
 
 def pop_with_args(dog_breeds)
@@ -33,6 +33,7 @@ end
 def using_concat(my_favorite_things, more_favs)
   my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
   more_favs = ["sports cars", "flatiron school"]
+  my_favorite_things.concat(["sports cars", "flatiron school"])
   my_favorite_things + more_favs
 end
 
