0b582fc1ceab89b61ef69d69084244631815c852
Russel Pugh <russ_scooby@yahoo.com>
2020-02-28 05:07:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -44,7 +44,7 @@ end
 
 def using_uniq(haircuts)
   haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
-  new_array = haircuts.uniq
+  haircuts.uniq
 end
 
 def using_flatten(array)
