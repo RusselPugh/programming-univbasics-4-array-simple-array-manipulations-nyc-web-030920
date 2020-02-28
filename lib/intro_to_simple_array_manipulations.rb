4df4e3dd864eea327ad976282535074dec2f1fd0
Russel Pugh <russ_scooby@yahoo.com>
2020-02-28 06:08:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,7 +29,6 @@ end
 def shift_with_args(ice_cream_brands)
   ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
   brands_removed = ice_cream_brands.shift(2)
-  ice_cream_brands
 end
 
 def using_concat(my_favorite_things, more_favs)
