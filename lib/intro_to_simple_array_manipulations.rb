99ffc6b1e112da955149d1ed91aab0ff63016e3b
Russel Pugh <russ_scooby@yahoo.com>
2020-02-28 05:22:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,10 +4,10 @@ def using_push(colors_in_the_rainbow,next_color)
   colors_in_the_rainbow.push(next_color)
 end
 
-def using_unshift(bouroughs_in_nyc, "new neighborhood")
+def using_unshift(bouroughs_in_nyc, new_neighborhood)
   bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-    "new neighborhood" = "Staten Island"
-    bouroughs_in_nyc.unshift("new neighborhood")
+    new_neighborhood = "Staten Island"
+    bouroughs_in_nyc.unshift(new_neighborhood)
 end
 
 def using_pop(continents)
