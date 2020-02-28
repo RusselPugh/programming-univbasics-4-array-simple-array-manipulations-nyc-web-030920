be123768c1de8f8059a2c3c82e60081a1241e22d
Russel Pugh <russ_scooby@yahoo.com>
2020-02-28 04:57:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -37,7 +37,9 @@ def using_concat(my_favorite_things, more_favs)
 end
 
 def using_insert(array, element)
-  
+  list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+  another_language = "Python"
+  new_array = using_insert(@list_of_programming_languages, @another_language)
 end
 
 def using_uniq(array)
