9f76f3ca79181c62ddd65e88458a5e3740dacc90
Russel Pugh <russ_scooby@yahoo.com>
2020-02-28 05:26:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -54,8 +54,7 @@ end
 
 def using_delete(instructors, no_offense_steven)
   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-  no_offense_steven = "Steven"
-  instructors.delete("no_offense_steven")
+  instructors.delete("Steven")
 end
 
 def using_delete_at(famous_robots, deleted_robot)
